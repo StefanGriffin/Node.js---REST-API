b93e7f15a4fe29ac6555aa5426103866b1c8e2c5
Stefan Griffin <stefg@Stefans-MacBook-Pro.local>
2021-10-10 12:36:37 -0400

    Project Setup 2

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,16 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post' }],
+    posts: [
+      {
+        title: 'First Post',
+        content: 'This is the first post',
+        imageUrl: 'images/truck.jpg',
+        creator: {
+          name: 'Stefan'
+        },
+        createdAt: new Date()
+      },
+    ],
   });
 };
 
